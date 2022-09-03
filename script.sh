8ebabdae4969abd710ff3cc623d5e6f820da9255
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-09-03 23:09:17 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/zsh
 
+brew update
+brew install node
+pip3 install virtualenv
+
+git clone https://github.com/mozilla-mobile/firefox-ios.git
+
+cd firefox-ios
+git checkout vXX.X
+sh ./bootstrap.sh
+
+
+
